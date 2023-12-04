116ca9ecabb6dd8cfd36b026e785c058569d815f
thejopemoba <joaomonte19761@gmail.com>
2023-12-04 16:08:21 -0300

    criando funcoes para executar as midias do projeto

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+function tocaPom(){
+    document.querySelector('#som_tecla_pom'). play();
+}
+
+function tocaClap(){
+    document.querySelector('#som_tecla_clap'). play();
+}
+
 document.querySelector('.tecla_pom');
